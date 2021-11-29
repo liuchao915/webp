0aaa52fd9076620e6726fcddd1e6fb2cf8931146
liuchao <liuchao@hinterstellar.com>
2021-11-29 16:31:15 +0800

    fix go mod vendor prune c files

---
+++ capi_dummy.go
@@ -0,0 +1,9 @@
+package webp
+
+import (
+	_ "github.com/chai2010/webp/internal/include"
+	_ "github.com/chai2010/webp/internal/libwebp"
+	_ "github.com/chai2010/webp/internal/libwebp-0.5.0"
+	_ "github.com/chai2010/webp/internal/libwebp-1.0.2"
+	_ "github.com/chai2010/webp/internal/src"
+)
